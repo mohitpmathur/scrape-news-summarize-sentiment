385f7be5705362ee8f53c0e232994b1014177ab0
mohitpmathur <mohitpmathur@gmail.com>
2016-07-20 23:53:31 -0400

    Counting positive/negative words

--- parse_news.py
+++ parse_news.py
@@ -1,6 +1,7 @@
 # Parse leading indian newsDict sites, and check sentiment
 
 import os
+import re
 from datetime import datetime
 import urllib2
 from bs4 import BeautifulSoup
@@ -9,6 +10,7 @@ from textblob import TextBlob
 from nltk.corpus import stopwords
 stopword = stopwords.words('english')
 
+
 def sentiment(text):
     '''
     Retruns sentiment of the text in the range -1 to 1
@@ -16,10 +18,65 @@ def sentiment(text):
 
     returns: fraction (sentiment) in range -1 to 1
     '''
-    #remove stopwords
+    # remove stopwords
     text = [t for t in text.split(" ") if t not in stopword]
     text = " ".join(w for w in text)
     return TextBlob(text).sentiment.polarity
+
+
+def readWordFiles(positiveFile, negativeFile):
+    positiveWords = []
+    negativeWords = []
+    pos = open(positiveFile, "r")
+    for line in pos:
+        if line.startswith(";") or line.strip() == "":
+            continue
+        positiveWords.append(line.strip())
+    pos.close()
+    negfhd = open(negativeFile, "r")
+    for line in negfhd:
+        if line.startswith(";") or line.strip() == "":
+            continue
+        negativeWords.append(line.strip())
+    negfhd.close()
+    return positiveWords, negativeWords
+
+
+def removePunctuation(text):
+    """Removes punctuation, changes to lower case, and strips leading
+    and trailing spaces.
+
+    Note:
+        Only spaces, letters, and numbers should be retained.  Other
+        characters should should be eliminated (e.g. it's becomes its).
+        Leading and trailing spaces should be removed after punctuation
+        is removed.
+
+    Args:
+        text (str): A string.
+
+    Returns:
+        str: The cleaned up string.
+    """
+    text = text.lower()
+    text = re.sub('[^A-Za-z0-9 ]+', '', text)
+    text = text.strip()
+    return text
+
+
+def get_sentiment(text, pos, neg):
+    posCount = 0
+    negCount = 0
+    text = [t for t in removePunctuation(text).split(" ") if t not in stopword]
+    for w in text:
+        if w in pos:
+            print "Positive: ", w
+            posCount += 1
+        if w in neg:
+            print "Negative: ", w
+            negCount += 1
+    return text, posCount, negCount
+
 
 def getHinduArticleText(url):
     '''
@@ -31,34 +88,36 @@ def getHinduArticleText(url):
     '''
     content = ""
     site = urllib2.urlopen(url).read()
-    #h = site.decode('utf-8', 'ignore')
+    # h = site.decode('utf-8', 'ignore')
     soup = BeautifulSoup(site)
-    #print soup.original_encoding
-    myDivs = soup.findAll("div", {"class" : "article-text"})
+    # print soup.original_encoding
+    myDivs = soup.findAll("div", {"class": "article-text"})
     if len(myDivs) == 0:
-        myDivs = soup.findAll("div", {"itemprop" : "articleBody"})
+        myDivs = soup.findAll("div", {"itemprop": "articleBody"})
     if len(myDivs) > 0:
-        contentPara = myDivs[0].findAll('p', {"class" : "body"})
+        contentPara = myDivs[0].findAll('p', {"class": "body"})
         for para in contentPara:
-            #print para.get_text().strip()
+            # print para.get_text().strip()
             content += para.get_text().strip()
     return content
 
-def parse_hindu(newsKeys = []):
+
+def parse_hindu(newsKeys=[]):
     site = urllib2.urlopen("http://www.thehindu.com/").read()
     soup = BeautifulSoup(site)
-    mydivs = soup.findAll("div", {"class" : "main-content mTop10"})
-    contentdivs = mydivs[0].findAll('div', {"data-vr-contentbox" : ""})
+    mydivs = soup.findAll("div", {"class": "main-content mTop10"})
+    contentdivs = mydivs[0].findAll('div', {"data-vr-contentbox": ""})
 
-    count = 0    
+    count = 0
     newsDict = {}
     for tag in contentdivs:
-        #htags = tag.findAll(['h1', 'h3'])
-        #for htag in htags:
+        # htags = tag.findAll(['h1', 'h3'])
+        # for htag in htags:
         atags = tag.findAll('a', href=True)
         for atag in atags:
             news_info = []
-            news_info.append(pd.to_datetime(datetime.strftime(datetime.now(), "%Y-%m-%d %H")))
+            news_info.append(pd.to_datetime(datetime.strftime(datetime.now(),
+                                            "%Y-%m-%d %H")))
             news_info.append('The Hindu')
             if atag.has_attr('data-vr-excerpttitle') and atag.get_text().strip() != "": #atag.string is not None:
                 #print atag.get_text().strip(), atag['href']
@@ -81,6 +140,10 @@ def parse_hindu(newsKeys = []):
     print "Number of unique newsDict items:",count
     return newsDict
 
+POSITIVE_FILE = "data\\positive-words.txt"
+NEGATIVE_FILE = "data\\negative-words.txt"
+
+'''
 newsDict = {}
 newsKeys = []
 newsDataFile = 'news_parser.pkl'
@@ -100,3 +163,4 @@ if len(newsDict) > 0:
     temp_df['URL_Content'] = temp_df['URL'].map(getHinduArticleText)
     df = pd.concat([df,temp_df], ignore_index=True)
     df.to_pickle(newsDataFile)
+'''
